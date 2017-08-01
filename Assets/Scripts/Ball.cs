6c1b44dcc6ec0950e72d70d451dbc4f7552d2893
Ryan Morgan <Morganglassworks@gmail.com>
2017-08-01 16:30:08 -0700

    Touch UI elements added
    Added UI touch frame and arrows to nudge ball left and right at start

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Vector3 launchVelocity;
+
+	public bool inPlay = false;
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.useGravity = false;
+	}
+
+	public void Launch (Vector3 velocity) {
+		inPlay = true;
+		rigidBody.useGravity = true;
+		rigidBody.velocity = launchVelocity;
+
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play();
+	}
+
+}
