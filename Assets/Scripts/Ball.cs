9575c1b30f38ffeec9cb10026fdb81ca5cd2fb4a
Ryan Morgan <Morganglassworks@gmail.com>
2017-08-02 15:46:19 -0700

    ball reset nad score total
    ball and camera now reset to default position after the pins have settled, score text detects pins in bounding box and updates

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,13 +5,16 @@ public class Ball : MonoBehaviour {
 	public Vector3 launchVeclocity;
 	public bool inPlay = false;
 
+	private Vector3 ballStartPos;
 	private Rigidbody rigidBody;
 	private AudioSource audioSource;
-	
+
+
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
 		rigidBody.useGravity = false;
+		ballStartPos = transform.position;
 	}
 	
 	public void Launch (Vector3 velocity)
@@ -23,5 +26,13 @@ public class Ball : MonoBehaviour {
 
 		audioSource = GetComponent<AudioSource> ();
 		audioSource.Play ();
+	}
+
+	public void Reset () {
+		inPlay = false;
+		transform.position = ballStartPos;
+		rigidBody.velocity = Vector3.zero;
+		rigidBody.angularVelocity = Vector3.zero;
+		rigidBody.useGravity = false;
 	}
 }
